c230bed56de59269287bf47a465e90dc886419b4
Habib AlMawali <habib.almawali@gmail.com>
2020-11-28 15:37:18 +0400

    done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,12 @@ export class HomePage {
 
   constructor() {}
 
+  option = {
+    slidesPerView: 1.5,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 10,
+    // autoplay:true,
+  }
+
 }
